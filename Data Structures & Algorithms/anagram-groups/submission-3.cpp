e7fc55200f5781b8a241e909d0997530d69be418
Vaibhav-Kumar-11 <24b0003@iitb.ac.in>
2026-06-17 18:23:21 +0530

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> mpp;
+        for(auto& it:strs){
+            string key = it;
+            sort(key.begin(),key.end());
+            mpp[key].push_back(it);
+        }
+
+        vector<vector<string>> ans;
+        for(auto& [key,group]:mpp){
+            ans.push_back(group);
+        }
+        return ans;
+    }
+};
