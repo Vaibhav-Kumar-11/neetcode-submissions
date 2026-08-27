1e37f2d3fd5cbe253aeb42835561ece692239d34
Vaibhav-Kumar-11 <24b0003@iitb.ac.in>
2026-08-27 22:24:27 +0530

    Add: meeting-schedule - submission-2

---
+++ Data Structures & Algorithms/meeting-schedule/submission-2.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](auto& a, auto& b) {
+        return a.start < b.start;
+        });
+
+        int endtime = intervals[0].end;
+        int n = intervals.size();
+
+        for(int i = 1; i < n; i++) {
+            if(intervals[i].start < endtime)
+                return false;
+
+            endtime = intervals[i].end;
+        }
+
+        return true;
+    }
+};
